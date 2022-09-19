42c8d31aca804baa97811815364caff2ddc86908
Fabiano Franca <fabianofranca.ti@gmail.com>
2022-09-19 10:05:16 -0300

    ajustando layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { FiCalendar, FiUser } from "react-icons/fi";
 
 interface Post {
   uid?: string;
@@ -24,9 +26,26 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return(
+    <>
+      <main className={styles.contentContainer}>
+        <section className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <h5>Pensando em sincronização em vez de ciclos de vida.</h5>
+
+          <div>
+            <FiCalendar className={styles.fi}/>
+            <span>15 Mar 2021</span>
+            
+            <FiUser className={styles.fi}/>
+            <span>Joseph Oliveira</span>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
